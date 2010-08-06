f7a9cea32f35cf6abb7028f54e00bf04a4f8935d
Carel Jonkhout <carel.jonkhout@gmail.com>
2010-08-06 17:15:58 -0400

    updated run/add_questions.rb

--- run/add_questions.rb
+++ run/add_questions.rb
@@ -1,8 +1,8 @@
 question = nil
-# must disable validate :must_have_an_answer in question.rb
-IO.foreach 'questions' do |line| 
+IO.foreach '/home/carel/ror/linuxquestions/org/run/questions' do |line|
   if line =~ /^q/ 
-    question = Question.create!(:question => line[1..line.length])
+    question = Question.new(:question => line[1..line.length])
+    question.save false # disabling validations because there are no answers specified yet.
   elsif line =~ /^t/
     question.answers.create!(:answer => line[1..line.length], :correct => true)
   elsif line =~ /^f/
