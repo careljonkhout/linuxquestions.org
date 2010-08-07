0ea2b4495da6b4aefb4739f48036e4e7d93813db
Carel Jonkhout <carel.jonkhout@gmail.com>
2010-08-07 16:14:26 -0400

    -m

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,5 +2,14 @@ class QuizzesController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all
+  auto_actions :all, :except => :show
+
+  def show
+    hobo_show do
+      unless current_user.signed_up?
+        flash[:notice] = 'Please sign up to take a quiz.'
+        redirect_to user_signup_path
+      end
+    end
+  end
 end
