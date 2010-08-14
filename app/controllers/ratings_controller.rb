aa52ecfadf16ee7403ba06aa405e29405ef2d974
Carel Jonkhout <carel.jonkhout@gmail.com>
2010-08-14 03:03:00 -0400

    adf

--- app/controllers/ratings_controller.rb
+++
@@ -1,5 +0,0 @@
-class RatingsController < ApplicationController
-
-  hobo_model_controller
-
-end
--- db/migrate/20100730172013_rename_answered_questions.rb
+++
@@ -1,9 +0,0 @@
-class RenameAnsweredQuestions < ActiveRecord::Migration
-  def self.up
-    rename_table :answered_questions, :responses
-  end
-
-  def self.down
-    rename_table :responses, :answered_questions
-  end
-end
